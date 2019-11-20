2ddef70f4d0622d8b3696b05a42d4a14673e816d
UnityLearnerr <403892921@qq.com>
2019-11-20 19:56:50 +0800

    修改LeetCode结构

--- 225.StackRealizeQueue/StackRealizeQueue/Program.cs
+++ 225.StackRealizeQueue/StackRealizeQueue/Program.cs
@@ -23,6 +23,55 @@ namespace StackRealision
         }
     }
 
+    public class MyStack
+    {
+        private Queue<int> m_Queue = null;
+        /** Initialize your data structure here. */
+        public MyStack()
+        {
+            m_Queue = new Queue<int>();
+        }
+
+        /** Push element x onto stack. */
+        public void Push(int x)
+        {
+            Queue<int> tempQueue = new Queue<int>();
+            tempQueue.Enqueue(x);
+
+            while (m_Queue.Count > 0)
+            {
+                int temp = m_Queue.Dequeue();
+                tempQueue.Enqueue(temp);
+            }
+            m_Queue = tempQueue;
+        }
+
+        /** Removes the element on top of the stack and returns that element. */
+        public int Pop()
+        {
+            if (m_Queue.Count == 0)
+                return default(int);
+            else
+                return m_Queue.Dequeue();
+        }
+
+        /** Get the top element. */
+        public int Top()
+        {
+            if (m_Queue.Count == 0)
+                return default(int);
+            else
+                return m_Queue.Peek();
+        }
+
+        /** Returns whether the stack is empty. */
+        public bool Empty()
+        {
+            return m_Queue.Count <= 0;
+        }
+    }
+
+
     public class StackRealizeQueue<T>
     {
         private Queue<T> m_Queue = null;
