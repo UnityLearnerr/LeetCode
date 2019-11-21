0da5b5074980a6c7444af5e447e106e054e08eba
UnityLearnerr <403892921@qq.com>
2019-11-21 15:26:36 +0800

    232.用栈实现队列

---
+++ 232.QueueRealizeByStack/QueueRealizeByStack/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueRealizeByStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue queue = new MyQueue();
+            queue.Push(1);
+            queue.Push(2);
+            Console.WriteLine(queue.Peek());
+        }
+    }
+
+    public class MyQueue
+    {
+        public Stack<int> m_Stack = null;
+        /** Initialize your data structure here. */
+        public MyQueue()
+        {
+            m_Stack = new Stack<int>();
+        }
+
+        /** Push element x to the back of queue. */
+        public void Push(int x)
+        {
+            m_Stack.Push(x);
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int Pop()
+        {
+            Stack<int> tempStack = new Stack<int>();
+            while (m_Stack.Count > 0) 
+            {
+                int temp = m_Stack.Pop();
+                tempStack.Push(temp);
+            }
+            int result = tempStack.Pop();
+            while (tempStack.Count > 0) 
+            {
+                int temp = tempStack.Pop();
+                m_Stack.Push(temp);
+            }
+            return result;
+        }
+
+        /** Get the front element. */
+        public int Peek()
+        {
+            Stack<int> tempStack = new Stack<int>();
+            while (m_Stack.Count > 0)
+            {
+                int temp = m_Stack.Pop();
+                tempStack.Push(temp);
+            }
+            int result = tempStack.Peek();
+            while (tempStack.Count > 0)
+            {
+                int temp = tempStack.Pop();
+                m_Stack.Push(temp);
+            }
+            return result;
+        }
+
+        /** Returns whether the queue is empty. */
+        public bool Empty()
+        {
+            return m_Stack.Count <= 0;
+        }
+    }
+}
