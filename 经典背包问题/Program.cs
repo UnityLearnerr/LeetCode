65970dab970d217679f2887d5ec582b324bd1501
lizheng <403892921@qq.com>
2024-06-26 20:08:26 +0800

    经典背包

---
+++ 经典背包问题/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int r = Dps(0, 10);
+        Console.WriteLine(r);
+    }
+
+    private static int[] weights = new int[] { 5, 2, 4, 4, 5 };
+    private static int[] values = new int[] { 4, 1, 2, 3, 4 };
+
+
+    private static int Dps(int index, int residue)
+    {
+        if (index >= weights.Length)
+        {
+            return 0;
+        }
+        if (residue < weights[index])
+        {
+            return 0;
+        }
+        return Math.Max(values[index] + Dps(index + 1, residue - weights[index]), Dps(index + 1, residue));
+    }
+}
