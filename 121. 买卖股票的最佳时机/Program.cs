3ed58b63bbeb7eb4f732b3700c2e44312a732b94
UnityLearnerr <403892921@qq.com>
2024-12-22 17:04:46 +0800

    121. 买卖股票的最佳时机 一次遍历就可以

--- 121. 买卖股票的最佳时机/Program.cs
+++ 121. 买卖股票的最佳时机/Program.cs
@@ -6,12 +6,37 @@ namespace _121._买卖股票的最佳时机
     {
         static void Main(string[] args)
         {
-            int[] prices = new int[] { 7, 6, 4, 3, 1 };
+            int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
             int result = MaxProfit(prices);
             Console.WriteLine(result);
         }
 
-        public static int MaxProfit(int[] prices)
+        /// <summary>
+        /// 一遍遍历就够了
+        /// </summary>
+        /// <param name="prices"></param>
+        /// <returns></returns>
+        public static int MaxProfit(int[] prices) 
+        {
+            int minPrice = prices[0];
+            int maxProfit = 0;
+            for (int i = 0, imax = prices.Length; i < imax; i++) 
+            {
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
+                    continue;
+                }
+                int profit = prices[i] - minPrice;
+                if (profit > maxProfit) 
+                {
+                    maxProfit = profit;
+                }
+            }
+            return maxProfit;
+        }
+
+        public static int MaxProfit1(int[] prices)
         {
             int minPrice = int.MaxValue;
             int minPriceIndex = 0;
@@ -33,8 +58,6 @@ namespace _121._买卖股票的最佳时机
                     maxProfit = profit;
                 }
             }
-
-
             return maxProfit;
         }
     }
