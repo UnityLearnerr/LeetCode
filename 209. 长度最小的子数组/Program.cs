98c671d9b76b53b2d51be7a426b8d4a2400ba56f
lizheng <403892921@qq.com>
2024-12-24 15:34:35 +0800

    209. 长度最小的子数组

---
+++ 209. 长度最小的子数组/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _209._长度最小的子数组
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 3, 1, 2, 4, 3 };
+            int result =  MinSubArrayLen(7, nums);
+            Console.WriteLine(result);
+        }
+
+        public static int MinSubArrayLen(int target, int[] nums)
+        {
+            int start = 0;
+            int end = 0;
+            int sum = 0;
+            int result = int.MaxValue;
+
+            while (end < nums.Length)
+            {
+                sum += nums[end];
+                while (sum >= target)
+                {
+                    result = Math.Min(end - start + 1, result);
+                    sum -= nums[start];
+                    start++;
+                }
+                end++;
+            }
+            return result <= nums.Length ? result : 0;
+        }
+    }
+}
