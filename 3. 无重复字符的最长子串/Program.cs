136e740c1a7fe584c09848c9a5886261bb48ea35
lizheng <403892921@qq.com>
2024-12-24 16:43:00 +0800

    3._无重复字符的最长子串

---
+++ 3. 无重复字符的最长子串/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace _3._无重复字符的最长子串
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "abcabcbb";
+            int result =  LengthOfLongestSubstring(str);
+            Console.WriteLine(result);
+        }
+
+        public static int LengthOfLongestSubstring(string s)
+        {
+            int start = 0;
+            int end = 0;
+            int result = 0;
+            HashSet<char> record = new HashSet<char>();
+            while (end < s.Length)
+            {
+                while (record.Contains(s[end])) 
+                {
+                    record.Remove(s[start]);
+                    start++;
+                }
+                result = Math.Max(end - start + 1, result);
+                record.Add(s[end]);
+                end++;
+            }
+            return result;
+        }
+    }
+}
