f43b006f3d64b4cff5d71a17b18a77ef0f8b75e4
lizheng <403892921@qq.com>
2024-12-25 21:15:01 +0800

    409. 最长回文串 另外一种写法

--- 409. 最长回文串/Program.cs
+++ 409. 最长回文串/Program.cs
@@ -4,17 +4,52 @@
     {
         static void Main(string[] args)
         {
-            int num = LongestPalindrome("abccccdd");
+            int num = LongestPalindrome1("bananas");
             Console.WriteLine(num);
         }
+
+        public static int LongestPalindrome1(string s)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            int palindromeLenghth = 0;
+            for (int i = 0, imax = s.Length; i < imax; i++)
+            {
+                if (!dic.ContainsKey(s[i]))
+                {
+                    dic.Add(s[i], 0);
+                }
+                dic[s[i]] += 1;
+            }
+
+            foreach (int count in dic.Values)
+            {
+                if ((count & 1) == 1)
+                {
+                    if ((palindromeLenghth & 1) == 0)
+                    {
+                        palindromeLenghth += count;
+                    }
+                    else 
+                    {
+                        palindromeLenghth += (count & (int.MaxValue - 1));
+                    }
+                }
+                else
+                {
+                    palindromeLenghth += count;
+                }
+            }
+            return palindromeLenghth;
+        }
+
 
         public static int LongestPalindrome(string s)
         {
             Dictionary<char, int> dic = new Dictionary<char, int>();
             int palindromeLenghth = 0;
-            for (int i = 0, imax = s.Length; i < imax; i++) 
+            for (int i = 0, imax = s.Length; i < imax; i++)
             {
-                if (!dic.ContainsKey(s[i])) 
+                if (!dic.ContainsKey(s[i]))
                 {
                     dic.Add(s[i], 0);
                 }
@@ -22,10 +57,10 @@
                 dic[s[i]] += 1;
             }
 
-            foreach (int count in dic.Values) 
+            foreach (int count in dic.Values)
             {
                 palindromeLenghth += count / 2 * 2;
-                if ((count & 1) == 1 && (palindromeLenghth & 1) == 0) 
+                if ((count & 1) == 1 && (palindromeLenghth & 1) == 0)
                 {
                     palindromeLenghth++;
                 }
