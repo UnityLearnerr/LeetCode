2a7d943dca87f36934859ed47682ce643b48526f
lizheng <403892921@qq.com>
2024-12-25 17:49:48 +0800

    136. 只出现一次的数字

---
+++ 136. 只出现一次的数字/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _136._只出现一次的数字
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { };
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int SingleNumber(int[] nums)
+        {
+            int result = 0;
+            for (int i = 0,imax = nums.Length; i < imax; i++) 
+            {
+                result ^= nums[i];
+            }
+            return result;
+        }
+    }
+}
